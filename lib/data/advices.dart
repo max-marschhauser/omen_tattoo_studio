4cfbbde73ca1bd360cd1e81bd92ee884c49c4ca8
Max Marschhauser <max.marschhauser@gmail.com>
2024-07-11 18:45:24 +0200

    Update advices.dart

--- lib/data/advices.dart
+++ lib/data/advices.dart
@@ -1,8 +1,8 @@
 import '../models/advice.dart';
 
 List<OMAdvice> advicesList = [
-  OMAdvice(title: "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1", content: "content1", category: OMAdviceCategory.priprema),
-  OMAdvice(title: "title2", content: "title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1title1", category: OMAdviceCategory.tijekom),
+  OMAdvice(title: "title1", content: "content1", category: OMAdviceCategory.priprema),
+  OMAdvice(title: "title2", content: "content2", category: OMAdviceCategory.tijekom),
   OMAdvice(title: "title3", content: "content3", category: OMAdviceCategory.njega),
   OMAdvice(title: "title4", content: "content4", category: OMAdviceCategory.njega),
   OMAdvice(title: "title5", content: "content5", category: OMAdviceCategory.njega),
